3e55d2d32da8abc5043f87a8a91b355f2ffd86c5
Courtney Topping <Courtney@Courtneys-MacBook-Pro.local>
2019-10-21 17:48:13 -0400

    node

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
 
-    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
+    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_cards";
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
         console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
-        res.render('home', { data: result });
+        //res.render('home', { data: result });
     })
 })
 
